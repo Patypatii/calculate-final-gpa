ba21613b9d1085dd1e006ac102ec79a0ae77d312
Patypatii <patrickwambugu707@gmail.com>
2025-07-11 22:28:30 +0300

    solved the challenge

--- Program.cs
+++ Program.cs
@@ -10,3 +10,19 @@ int course2Credit = 3;
 int course3Credit = 4;
 int course4Credit = 4;
 int course5Credit = 3;
+
+Console.WriteLine($"Student Name:  {studentName}");
+Console.WriteLine("Course\t\t\tGradeCredit Hours");
+
+Console.WriteLine($"{course1Name}\t\t\t8\t{course1Credit}");
+
+Console.WriteLine($"{course2Name}\t\t\t5\t{course2Credit}");
+
+Console.WriteLine($"{course3Name}\t\t\t7\t{course3Credit}");
+
+Console.WriteLine($"{course4Name}\t\t\t9\t{course4Credit}");
+
+Console.WriteLine($"{course5Name}\t\t\t1\t{course5Credit}");
+
+
+Console.WriteLine($"Final GPA: {course1Credit + course2Credit + course3Credit + course4Credit + course5Credit} / 5");
--- obj/Debug/net9.0/calculate-final-gpa.AssemblyInfo.cs
+++ obj/Debug/net9.0/calculate-final-gpa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calculate-final-gpa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b3452098796c7e911fb4ec2acee7dc775423395")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e851420475bd4987adab1135c7b3e0bdadf7ff40")]
 [assembly: System.Reflection.AssemblyProductAttribute("calculate-final-gpa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calculate-final-gpa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
